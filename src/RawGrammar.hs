f9f9ef76b7e8b0df3968f5c110480c43a2fe7b3d
Luis Alberto Díaz Díaz <work_luis23_45diaz@protonmail.com>
2021-08-09 12:09:29 -0500

    Added CompilerShow class

--- src/RawGrammar.hs
+++ src/RawGrammar.hs
@@ -3,6 +3,13 @@ module RawGrammar where
 
 import Data.Text(Text, pack, unpack)
 import qualified Data.Text as T
+import qualified Data.DList as D
+import Data.DList(DList)
 
 
+class CompilerShow a where
+  prettyShow :: a -> DList a
+  debugShow :: a -> DList a
+  reconstructShow:: a -> DList a
+  runShow :: DList a -> a
 
