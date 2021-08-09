c1da3ac1b957b745734671533302d72535db796a
Luis Alberto Díaz Díaz <work_luis23_45diaz@protonmail.com>
2021-08-09 12:48:47 -0500

    Moved runShow off of CompilerShow

--- src/RawGrammar.hs
+++ src/RawGrammar.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module RawGrammar where
 
-import Data.Text(Text, pack, unpack)
+import Prelude hiding(concat)
+import Data.Text(Text, pack, unpack, concat)
 import qualified Data.Text as T
 import qualified Data.DList as D
 import Data.DList(DList)
@@ -9,13 +10,16 @@ import Data.DList(DList)
 -- | Functions for ease of show
 class CompilerShow a where
   -- | A nice way to watch the code
-  prettyShow :: a -> DList a
+  prettyShow :: a -> DList Text
+  prettyShow = reconstructShow
   -- | Must be full of annotations and easy parseable
-  debugShow :: a -> DList a
+  debugShow :: a -> DList Text
+  debugShow = reconstructShow
   -- | Must attempt to reconstruct the object on file
-  reconstructShow:: a -> DList a
-  -- | in general don't needed but it could be used to add custom things at render
-  runShow :: DList a -> a
+  reconstructShow:: a -> DList Text
+  
+runShow :: CompilerShow a => (a-> DList Text) -> a -> Text
+runShow f = concat . D.toList . f  
 
 
 
