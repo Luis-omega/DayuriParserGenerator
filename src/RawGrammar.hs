9bbe811769e6de4f8c3d34443cd63f52302e4785
Luis Alberto Díaz Díaz <work_luis23_45diaz@protonmail.com>
2021-08-09 12:18:41 -0500

    Doc for CompilerShow

--- src/RawGrammar.hs
+++ src/RawGrammar.hs
@@ -6,10 +6,16 @@ import qualified Data.Text as T
 import qualified Data.DList as D
 import Data.DList(DList)
 
-
+-- | Functions for ease of show
 class CompilerShow a where
+  -- | A nice way to watch the code
   prettyShow :: a -> DList a
+  -- | Must be full of annotations and easy parseable
   debugShow :: a -> DList a
+  -- | Must attempt to reconstruct the object on file
   reconstructShow:: a -> DList a
+  -- | in general don't needed but it could be used to add custom things at render
   runShow :: DList a -> a
+
+
 
