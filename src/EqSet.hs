0d89f0abbcc48fe5db609be66e40023e32ecd25b
Luis Alberto Díaz Díaz <work_luis23_45diaz@protonmail.com>
2021-12-01 14:15:12 -0600

    Changed interface to one more simple

---
+++ src/EqSet.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+
+module EqSet(
+  EqSet()
+  ,toList
+  ,append
+  ,concat
+  ,fromList
+  ,elem
+  ,singleton
+  ,empty
+  ,difference
+  ,null
+  ,enumerate
+)where 
+
+import Data.Bifunctor(second)
+import Prelude hiding(concat, elem, null)
+import qualified Prelude as P
+
+newtype EqSet a = EqSet {list::[a]}
+  deriving (Eq, Functor, Foldable, Traversable, Show)
+
+
+instance Eq a => Semigroup (EqSet a) where 
+  (<>) = concat
+
+instance Eq a => Monoid (EqSet a) where 
+  mempty = empty
+
+
+toList :: EqSet a -> [a]
+toList = list
+
+append :: (Eq a) => a -> EqSet a -> EqSet a
+append x xs= 
+  if P.elem x (toList xs) then 
+    xs
+  else 
+      EqSet (x:list xs)
+
+concat :: (Eq a) => EqSet a -> EqSet a -> EqSet a
+concat (EqSet []) b = b
+concat a (EqSet []) = a
+concat a b = fromList $ toList a ++ toList b
+
+fromList :: Eq a => [a] -> EqSet a
+fromList = foldr append mempty
+
+elem :: Eq a => a -> EqSet a -> Bool
+elem e l = P.elem e $ toList l
+
+singleton x = EqSet [x]
+
+empty = EqSet []
+
+delete :: Eq a => a -> EqSet a -> EqSet a
+delete x rs = 
+    let new = filter (/=x) $ toList rs in 
+        EqSet new
+
+null (EqSet []) = True
+null (EqSet x) = False
+
+difference :: Eq a => EqSet a -> EqSet a  ->EqSet a
+difference x y = fromList $ filter (\z -> notElem z (toList y)) $ toList x
+
+enumerate :: EqSet a -> [(Int,a)]
+enumerate = enumerateList . toList
+  where 
+    enumerateList [] = []
+    enumerateList (x:xs) = 
+      let new = enumerateList xs in 
+        case new of 
+          [] -> [(1,x)]
+          ((n,y):ys) -> (n+1,x):new
